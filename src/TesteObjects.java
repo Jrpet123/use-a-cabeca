33326f4b01a29fda1f0be12f98f427b8e38e9045
José Petrucio <jrazevedo.lps@gmail.com>
2025-07-09 18:03:45 -0300

    adicionei o TesteObjects

---
+++ src/TesteObjects.java
@@ -0,0 +1,22 @@
+public class TesteObjects {
+
+    int idade;
+    String nome;
+    double altura;
+
+    public void criar() {
+        System.out.println("Idade: " + idade);
+        System.out.println("Nome: " + nome);
+        System.out.println("ALtura: " + altura);
+    }
+
+    public static void main(String[] args) {
+
+       TesteObjects pessoa = new TesteObjects();
+
+       pessoa.altura = 1.90;
+       pessoa.idade = 23;
+       pessoa.nome = "José";
+       pessoa.criar();
+    }
+}
